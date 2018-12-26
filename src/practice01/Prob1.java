3a01c0ccffd16e48bb50106c95a5f9fbac2574c2
CoRock <dycorphin@gmail.com>
2018-12-26 23:08:46 +0900

    Add Prob1.java

--- src/practice01/Prob1.java
+++ src/practice01/Prob1.java
@@ -5,7 +5,16 @@ import java.util.Scanner;
 public class Prob1 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner( System.in );
+		int num = 0;
 		
+		System.out.print("수를 입력하시오 : ");
+		num = scanner.nextInt();
+		
+		if (num != 0 && num % 3 == 0) {
+			System.out.println("3의 배수입니다.");
+			return;
+		}
+		System.out.println("3의 배수가 아닙니다.");
 		
 		scanner.close();
 	}
