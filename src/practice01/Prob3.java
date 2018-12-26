80aab0d3440f1b2e5a08bd3db4c09af0b8aa6443
CoRock <dycorphin@gmail.com>
2018-12-26 23:22:18 +0900

    Add Prob3.java

--- src/practice01/Prob3.java
+++ src/practice01/Prob3.java
@@ -1,7 +1,30 @@
 package practice01;
 
+import java.util.Scanner;
+
 public class Prob3 {
 	
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int num = 0;
+		int sum = 0;
+		
+		System.out.print("숫자를 입력하세요: ");
+		num = sc.nextInt();
+		
+		// if num is even
+		if (num % 2 == 0) {
+			for (int i = 0; i <= num; i += 2) {
+				sum += i;
+			}
+		} else {
+			for (int i = 1; i <= num; i += 2) {
+				sum += i;
+			}
+		}
+		System.out.print("결과 값 : ");
+		System.out.println(sum);
 	}
+	
 }
