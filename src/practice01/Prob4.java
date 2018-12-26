1f17d15f735fc1bebf082aead01ce2488c13759c
CoRock <dycorphin@gmail.com>
2018-12-26 23:35:10 +0900

    Add Prob4.java

--- src/practice01/Prob4.java
+++ src/practice01/Prob4.java
@@ -1,9 +1,21 @@
 package practice01;
 
+import java.util.Scanner;
 
 public class Prob4 {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = null;
+		int pointer = 1;
+		
+		System.out.print("문자열을 입력하세요. : ");
+		str = sc.nextLine();
+		
+		for (int i = 0; i < str.length(); i++) {
+			System.out.println(str.substring(0, pointer));
+			pointer++;
+		}
 	}
 
 }
